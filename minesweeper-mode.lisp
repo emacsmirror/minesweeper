a7d410e2a6a6bd6affe1243319d84d3ce5770fce
zck <unknown>
2010-07-29 00:00:07 -0400

    fleshed out 'minesweeper-mode, added hook and map.

--- minesweeper-mode.lisp
+++ minesweeper-mode.lisp
@@ -1,2 +1,18 @@
 (defun minesweeper-mode ()
-  "Minesweeper mode for Emacs.")
+  "Major mode for playing Minesweeper in Emacs."
+  (interactive)
+  (kill-all-local-variables)
+  (setq mode-name "Minesweeper")
+  (setq major-mode 'minesweeper-mode)
+  (use-local-map minesweeper-mode-map)
+  (run-hooks 'minesweeper-mode-hook))
+
+(provide 'minesweeper-mode)
+
+(defvar minesweeper-mode-hook nil)
+
+(defvar minesweeper-mode-map
+  (let ((minesweeper-mode-map (make-sparse-keymap)))
+    )
+  "keymap for Minesweeper major mode")
+
