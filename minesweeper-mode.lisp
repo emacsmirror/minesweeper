b946b06c69be8215474f657a832da3a228c8d2be
zck <unknown>
2010-07-30 00:45:03 -0400

    Added minesweeper-init, variables for height, width, number of mines.

--- minesweeper-mode.lisp
+++ minesweeper-mode.lisp
@@ -1,18 +1,36 @@
-(defun minesweeper-mode ()
-  "Major mode for playing Minesweeper in Emacs."
-  (interactive)
+(define-derived-mode minesweeper-mode nil "Minesweeper" "Major mode for playing Minesweeper in Emacs.
+\\{minesweeper-mode-map}"
   (kill-all-local-variables)
-  (setq mode-name "Minesweeper")
-  (setq major-mode 'minesweeper-mode)
   (use-local-map minesweeper-mode-map)
-  (run-hooks 'minesweeper-mode-hook))
+  (toggle-read-only t))
 
-(provide 'minesweeper-mode)
+(defvar minesweeper-board-width nil
+  "The number of columns on the Minesweeper field.")
 
-(defvar minesweeper-mode-hook nil)
+(defvar minesweeper-board-height nil
+  "The number of rows on the Minesweeper field.")
 
-(defvar minesweeper-mode-map
-  (let ((minesweeper-mode-map (make-sparse-keymap)))
-    )
-  "keymap for Minesweeper major mode")
+(defvar minesweeper-mines nil
+  "The number of mines on the Minesweeper field.")
+
+(defun minesweeper-init (width height mines)
+  "Begin a game of Minesweeper with a board that's 'width by 'height size containing 'mines mines."
+  (setq minesweeper-board-width width
+	minesweeper-board-height height
+	minesweeper-mines mines)
+  (fill-minefield))
+
+
+  ;; (kill-all-local-variables)
+  ;; (use-local-map minesweeper-mode-map)
+  ;; (run-hooks 'minesweeper-mode-hook))
+
+;; (provide 'minesweeper-mode)
+
+;; (defvar minesweeper-mode-hook nil)
+
+;; (defvar minesweeper-mode-map
+;;   (let ((minesweeper-mode-map (make-sparse-keymap)))
+;;     )
+;;   "keymap for Minesweeper major mode")
 
