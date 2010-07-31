9d13c19d7f72f0d2b5b5f8aface6f6e77ab27a96
zck <unknown>
2010-07-31 01:07:59 -0400

    variables now have default arguments; they don't need to be set in 'minesweeper-init. Added 'minesweeper-field. It's a hashtable 'minesweeper-init now takes no arguments, for simplicity's sake. This could change later. 'minesweeper-fill-field fills the field properly, even if it doesn't build the neighbor count yet. Added functions to get and set squares.

--- minesweeper-mode.lisp
+++ minesweeper-mode.lisp
@@ -4,22 +4,52 @@
   (use-local-map minesweeper-mode-map)
   (toggle-read-only t))
 
-(defvar minesweeper-board-width nil
+(defvar minesweeper-board-width 10
   "The number of columns on the Minesweeper field.")
 
-(defvar minesweeper-board-height nil
+(defvar minesweeper-board-height 10
   "The number of rows on the Minesweeper field.")
 
-(defvar minesweeper-mines nil
+(defvar minesweeper-mines 10
   "The number of mines on the Minesweeper field.")
 
-(defun minesweeper-init (width height mines)
-  "Begin a game of Minesweeper with a board that's 'width by 'height size containing 'mines mines."
-  (setq minesweeper-board-width width
-	minesweeper-board-height height
-	minesweeper-mines mines)
-  (fill-minefield))
+(defvar minesweeper-field nil
+  "The minefield itself. If a mine is in the square, 'X is stored. Otherwise, the number of mines in neighboring squares is stored.")
 
+(defun minesweeper-init ()
+  "Begin a game of Minesweeper with a board that's 'width by 'height size containing 'mines mines."
+  (setq ;; minesweeper-board-width width
+	;; minesweeper-board-height height
+	;; minesweeper-mines mines
+	minesweeper-field (make-hash-table :test eql)
+  (minesweeper-fill-field)
+  (minesweeper-display-field)))
+
+
+(defun minesweeper-fill-field ()
+  "Fills 'minesweeper-field with 'minesweeper-mines mines, and builds the neighbor count."
+  (let ((mines-to-insert minesweeper-mines))
+    (while (> mines-to-insert 0)
+	   (let ((x (random minesweeper-board-width))
+		 (y (random minesweeper-board-height)))
+		 (unless (eq (minesweeper-view-mine x y)
+			     'X)
+		   (minesweeper-set-mine x y 'X)
+		   ;; (setq (minesweeper-view-mine x y)
+		   ;;    'X)
+		   ;; (minesweeper-inform-around x y)
+		   (setq mines-to-insert (1- mines-to-insert)))))))
+
+(defun minesweeper-view-mine (x y)
+  "Returns the value at position (x, y), where the origin is the upper left corner of the minefield."
+  (gethash (list x y)
+	   minesweeper-field))
+
+(defun minesweeper-set-mine (x y val)
+  "Inserts val into the mine at (x, y)"
+  (puthash (list x y)
+	   val
+	   minesweeper-field))
 
   ;; (kill-all-local-variables)
   ;; (use-local-map minesweeper-mode-map)
