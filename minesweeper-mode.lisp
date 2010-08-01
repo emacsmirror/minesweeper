0083bddb386d35170656d0f0d2b26308d8fef1f4
zck <unknown>
2010-08-01 02:19:50 -0400

    Added 'minesweeper-filter variable, 'minesweeper-set-filter, 'minesweeper-print-field, 'minesweeper-to-character, 'for macro. Also, 'minesweeper-fill-field now puts zeros into each space before it does anything else. 'minesweeper-view-mine now takes an optional 'reveal argument, so that it returns ?_ for any mine that hasn't been revealed yet.

--- minesweeper-mode.lisp
+++ minesweeper-mode.lisp
@@ -16,18 +16,26 @@
 (defvar minesweeper-field nil
   "The minefield itself. If a mine is in the square, 'X is stored. Otherwise, the number of mines in neighboring squares is stored.")
 
+(defvar minesweeper-filter nil
+  "Holds 't in (x, y) if (x, y) has been revealed")
+
 (defun minesweeper-init ()
   "Begin a game of Minesweeper with a board that's 'width by 'height size containing 'mines mines."
   (setq ;; minesweeper-board-width width
 	;; minesweeper-board-height height
 	;; minesweeper-mines mines
-	minesweeper-field (make-hash-table :test eql)
+   minesweeper-field (make-hash-table :test 'equal)
+   minesweeper-filter (make-hash-table :test 'equal))
   (minesweeper-fill-field)
-  (minesweeper-display-field)))
+  (minesweeper-print-field))
 
 
 (defun minesweeper-fill-field ()
   "Fills 'minesweeper-field with 'minesweeper-mines mines, and builds the neighbor count."
+  (for x 0 minesweeper-board-width
+       (for y 0 minesweeper-board-height
+	    (minesweeper-set-mine x y 0)
+	    (minesweeper-set-filter x y nil)))
   (let ((mines-to-insert minesweeper-mines))
     (while (> mines-to-insert 0)
 	   (let ((x (random minesweeper-board-width))
@@ -40,16 +48,26 @@
 		   ;; (minesweeper-inform-around x y)
 		   (setq mines-to-insert (1- mines-to-insert)))))))
 
-(defun minesweeper-view-mine (x y)
-  "Returns the value at position (x, y), where the origin is the upper left corner of the minefield."
-  (gethash (list x y)
-	   minesweeper-field))
+(defun minesweeper-view-mine (x y &optional reveal)
+  "If reveal is true, or if the selected mine has been revealed, returns the value at position (x, y), where the origin is the upper left corner of the minefield. Otherwise, it returns '_'"
+  (if (or reveal
+	  (gethash (list x y)
+		   minesweeper-filter))
+      (gethash (list x y)
+	       minesweeper-field)
+    ?\_))
 
 (defun minesweeper-set-mine (x y val)
   "Inserts val into the mine at (x, y)"
   (puthash (list x y)
 	   val
 	   minesweeper-field))
+
+(defun minesweeper-set-filter (x y val)
+  "Sets (x, y) in the filter to 'val"
+  (puthash (list x y)
+	   val
+	   minesweeper-filter))
 
 (defun minesweeper-inform-around (x y)
   "takes in a square, and increases the values of all its empty neighbors"
@@ -67,7 +85,7 @@
 (defun minesweeper-neighbors (x y)
   "Returns a list of the neighbors of (x, y)."
   (let ((neighbors nil))
-    (for newx (1- x) (1+ x) ;; make for loops work in Elisp
+    (for newx (1- x) (1+ x)
 	 (for newy (1- y) (1+ y)
 	      (unless (or (and (eq newx x)
 			       (eq newy y))
@@ -75,9 +93,36 @@
 			  (< newy 0)
 			  (>= newx minesweeper-board-width)
 			  (>= newy minesweeper-board-height))
-		(push (list x y)
+		(push (list newx newy)
 		      neighbors))))
     neighbors))
+
+(defun minesweeper-print-field ()
+  "Print out the minefield."
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (goto-char (point-min))
+    (for x 0 (1- minesweeper-board-width)
+	 (for y 0 (1- minesweeper-board-height)
+	      (insert-char (minesweeper-to-character (minesweeper-view-mine x y)) 1))
+	 (newline))))
+
+(defun minesweeper-to-character (val)
+  "Takes in a number or 'X, and returns its printable character."
+  (cond ((eq val 'X)
+	 ?X)
+	((eq val ?_)
+	 ?_)
+	(t (+ ?0 val))))
+
+
+(defmacro for (var init end &rest body)
+  "helper function. executes 'body repeatedly, with 'var assigned values starting at 'init, and ending at 'end, increasing by one each iteration."
+  `(let ((,var ,init)
+	 (end-val ,end))
+     (while (<= ,var end-val)
+       ,@body
+       (setq ,var (1+ ,var)))))
 
 
   ;; (run-hooks 'minesweeper-mode-hook))
