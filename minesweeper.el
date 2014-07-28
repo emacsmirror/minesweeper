9cedabf55456976384b2f8c56c375f134c72a676
Zachary Kanfer <zkanfer@gmail.com>
2014-07-28 16:08:39 -0400

    Documentation updates to shut up flycheck -- two spaces after periods.

--- minesweeper.el
+++ minesweeper.el
@@ -25,8 +25,8 @@
 
 ;; This program is an implementation of minesweeper for Emacs.
 
-;; This came consists of a minefield. There are squares with mines, and squares without mines.
-;; The goal is to reveal all squares that do not have a mine. If you reveal a mine, it explodes,
+;; This game consists of a minefield.  There are squares with mines, and squares without mines.
+;; The goal is to reveal all squares that do not have a mine.  If you reveal a mine, it explodes,
 ;; and the game is over!
 
 ;; To begin playing, call `M-x minesweeper`.
@@ -38,9 +38,9 @@
 ;; When a square is revealed, if it is not a mine, there will be a number indicating the count of mines in the neighboring eight squares.
 ;; If a square with no neighboring mines is revealed, all its neighbors will also be revealed.
 
-;; You can mark squares that you think have mines in them. Marked squares are protected
+;; You can mark squares that you think have mines in them.  Marked squares are protected
 ;; from being revealed by any means.
-;; To mark a square, right-click on it, or press `m`. Marked squares can be unmarked the same way.
+;; To mark a square, right-click on it, or press `m`.  Marked squares can be unmarked the same way.
 
 ;; You can choose to reveal all the neighbors of a square by middle-clicking on a square, or moving point there and pressing `c`.
 
